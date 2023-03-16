878b2255bbb90c9c461acdd3ae77701f080e7465
Thomas Json <joogletom8@gmail.com>
2023-03-16 09:51:14 +0100

    Changes

--- src/screens/homeScreen/HomeScreen.jsx
+++ src/screens/homeScreen/HomeScreen.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const HomeScreen = () => {
-    return (
-        <>
-            Home
-        </>
-    );
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 };
 
 export default HomeScreen;
