8e145789bdb2e8fa24cbb53856d24b5b5314734d
ThomasJson <joogletom8@gmail.com>
2024-03-16 13:39:44 +0100

    Changes

--- src/screens/singleCategoryScreen/SingleCategoryScreen.jsx
+++ src/screens/singleCategoryScreen/SingleCategoryScreen.jsx
@@ -1,11 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import ProductCard from "../../components/productCard/ProductCard";
+import { useTranslation } from 'react-i18next';
+import { ThemeContext } from '../../contexts/ThemeContext';
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+import { MdNewReleases, MdTrendingUp } from "react-icons/md";
+import { BiDollar } from "react-icons/bi";
 
 const SingleCategoryScreen = () => {
 
   const { id } = useParams();
+  const { t } = useTranslation();
+  const { theme } = useContext(ThemeContext);
   const [products, setProducts] = useState([]);
+
+  const [sortPrice, setSortPrice] = useState(null);
+  const [sortNew, setSortNew] = useState(null);
+  const [sortBestOffer, setSortBestOffer] = useState(null);
+  const [sortBestSale, setSortBestSale] = useState(null);
 
   const fetchProducts = () => {
 
@@ -26,32 +38,91 @@ const SingleCategoryScreen = () => {
 
   }, [id]);
 
+  const resetSorts = () => {
+    setSortPrice(null);
+    setSortNew(null);
+    setSortBestOffer(null);
+    setSortBestSale(null);
+  };
+
+  const sortProducts = () => {
+    let sortedProducts = [...products];
+
+    if (sortPrice) {
+      sortedProducts.sort((a, b) => sortPrice === 'asc' ? a.price - b.price : b.price - a.price);
+    }
+    if (sortNew) {
+      sortedProducts.sort((a, b) => (a.new === b.new ? 0 : sortNew === 'asc' ? (a.new ? -1 : 1) : (a.new ? 1 : -1)));
+    }
+    if (sortBestOffer) {
+      sortedProducts.sort((a, b) => (a.bestOffer === b.bestOffer ? 0 : sortBestOffer === 'asc' ? (a.bestOffer ? -1 : 1) : (a.bestOffer ? 1 : -1)));
+    }
+    if (sortBestSale) {
+      sortedProducts.sort((a, b) => (a.bestSale === b.bestSale ? 0 : sortBestSale === 'asc' ? (a.bestSale ? -1 : 1) : (a.bestSale ? 1 : -1)));
+    }
+
+    return sortedProducts;
+  };
+
   return (
     <>
 
       <div className="flex flex-row w-full justify-between">
 
-        {/* <div className="hidden lg:flex flex-col w-24%">
+        <div className="hidden lg:flex flex-col w-24%">
 
-          <div className="p-2 border border-black">
-            <h2 className="mb-6">Filter your products :</h2>
+          <div className={`p-4 border rounded-lg ${theme.text} ${theme.bgPrimary}`}>
 
-            <div className="flex flex-col gap-1">
-              <p>Price</p>
-              <p>New</p>
-              <p>Offers</p>
-              <p>Trend</p>
+            <h2 className="mb-6 text-center text-xl font-Rubik">
+              {t('filter.filterTitle')}
+            </h2>
+
+            <div className="flex flex-col items-center gap-2 mb-1">
+
+              <button className={`w-49 flex flex-row justify-center items-center gap-1 p-2 border hover:bg-yellow-400 rounded-lg ${theme.textHover} ${theme.borderColor} hover:border-white`} onClick={() => { resetSorts(); setSortPrice(sortPrice === 'asc' ? 'desc' : 'asc') }}>
+                {t('filter.price')}
+
+                {sortPrice !== null && (
+                  sortPrice === 'asc' ?
+                    (<span className="mt-1"><IoIosArrowDown /></span>) :
+                    (<span><IoIosArrowUp /></span>)
+                )}
+              </button>
+
+              <button className={`w-49 flex flex-row justify-center items-center gap-1 p-2 border hover:bg-blue-400 rounded-lg ${theme.textHover} ${theme.borderColor} hover:border-white`} onClick={() => { resetSorts(); setSortNew(sortPrice === 'asc' ? 'desc' : 'asc') }}>
+                {t('filter.new')}
+
+                {sortNew !== null && (
+                  <span className="mt-1"><MdNewReleases /></span>
+                )}
+              </button>
+              <button className={`w-49 flex flex-row justify-center items-center gap-1 p-2 border hover:bg-orange-400 rounded-lg ${theme.textHover} ${theme.borderColor} hover:border-white`} onClick={() => { resetSorts(); setSortBestOffer(sortPrice === 'asc' ? 'desc' : 'asc') }}>
+                {t('filter.bestOffers')}
+
+                {sortBestOffer !== null && (
+                  <span className="mt-1"><BiDollar /></span>
+                )}
+              </button>
+              <button className={`w-49 flex flex-row justify-center items-center gap-1 p-2 border hover:bg-green-400 rounded-lg ${theme.textHover} ${theme.borderColor} hover:border-white`} onClick={() => { resetSorts(); setSortBestSale(sortPrice === 'asc' ? 'desc' : 'asc') }}>
+                {t('filter.bestSellers')}
+
+                {sortBestSale !== null && (
+                  <span className="mt-1"><MdTrendingUp /></span>
+                )}
+              </button>
+
             </div>
+
           </div>
 
-        </div> */}
+        </div>
 
-        <div className="flex flex-col w-full"> {/* lg:w-3/4 */}
+        <div className="flex flex-col w-full lg:w-3/4">
 
           <div className="w-full flex flex-row flex-wrap gap-4">
 
             {products.length > 0 &&
-              products?.map((product) => {
+              sortProducts()?.map((product) => {
 
                 return (
                   <ProductCard
